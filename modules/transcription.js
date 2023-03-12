f0afac1f50dc795b4a125ab5aac37777cc025a54
Yctan Laptop (ASUS TUF) <yiechertan@gmail.com>
2023-03-12 21:08:20 +0800

    Batch text detect images.

--- modules/transcription.js
+++ modules/transcription.js
@@ -1,10 +1,10 @@
 import vision from '@google-cloud/vision';
+import fs from 'fs'
 
 /**
- * processes the images -> transcriptions and store it in the DB
- * it is up to the client to poll for any updates
+ * for testing transcription
  */
-export async function processTranscription(job_id, img_b64s) {
+export async function processTranscriptionDemo(job_id, img_b64s) {
   console.log("Job: " + job_id);
   console.log("Total imgs: " + img_b64s.length);
   // blob
@@ -16,8 +16,38 @@ export async function processTranscription(job_id, img_b64s) {
   // detect text
   const client = new vision.ImageAnnotatorClient();
   const [result] = await client.textDetection(request);
-  const detections = result.textAnnotations;
-  console.log('Text:');
-  detections.forEach(text => console.log(text));
-  console.log('End text');
+  // write results to text file
+  // fs.writeFile('results.json', JSON.stringify(result), err => {
+  //   if (err) {
+  //     console.error(err);
+  //   }
+  // });
+}
+
+/**
+ * processes the images -> transcriptions and store it in the DB
+ * it is up to the client to poll for any updates
+ */
+export async function processTranscription(job_id, img_b64s) {
+  console.log("Job: " + job_id);
+  console.log("Total imgs: " + img_b64s.length);
+  // requests
+  const imgRequests = img_b64s.map((b64) => {
+    return {
+      image: { content: Buffer.from(b64, 'base64') },
+      features: [{type: 'TEXT_DETECTION'}]
+    }
+  });
+  const request = {
+    requests: imgRequests
+  };
+  // detect text
+  const client = new vision.ImageAnnotatorClient();
+  const [result] = await client.batchAnnotateImages(request);
+  // write results to text file
+  fs.writeFile('vision_data.json', JSON.stringify(result), err => {
+    if (err) {
+      console.error(err);
+    }
+  });
 }
