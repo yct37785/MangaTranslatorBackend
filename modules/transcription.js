ff2b2707f0721c8689544ecbddb47dfec5718f79
lumberman88 <yiechertan@gmail.com>
2023-03-14 10:43:50 +0800

    Vision API call in separate function.

--- modules/transcription.js
+++ modules/transcription.js
@@ -2,29 +2,50 @@ import vision from '@google-cloud/vision';
 import fs from 'fs'
 
 /**
- * processes the images -> transcriptions and store it in the DB
- * it is up to the client to poll for any updates
+ * vision OCR
  */
-export async function processTranscription(job_id, img_b64s) {
-  console.log("Job: " + job_id);
-  console.log("Total imgs: " + img_b64s.length);
+async function visionOCR(img_b64s) {
   // requests
   const imgRequests = img_b64s.map((b64) => {
     return {
       image: { content: Buffer.from(b64, 'base64') },
-      features: [{type: 'TEXT_DETECTION'}]
+      features: [{ type: 'TEXT_DETECTION' }]
     }
   });
   const request = {
     requests: imgRequests
   };
-  // detect text
-  const client = new vision.ImageAnnotatorClient();
-  const [result] = await client.batchAnnotateImages(request);
-  // write results to text file
-  fs.writeFile('data/vision_data_rawkuma.json', JSON.stringify(result), err => {
-    if (err) {
-      console.error(err);
+  // detect text Vision API
+  // const client = new vision.ImageAnnotatorClient();
+  // const [result] = await client.batchAnnotateImages(request);
+  // mock text data
+  const result = { responses: [] };
+  const data = fs.readFileSync('data/vision_data_rawkuma.json', 'utf8');
+  result.responses = JSON.parse(data).responses;
+  console.log(result.responses);
+  // write results to text file (debug)
+  // fs.writeFile('data/vision_data_rawkuma.json', JSON.stringify(result), err => {
+  //   if (err) {
+  //     console.error(err);
+  //   }
+  // });
+  return result.responses.map((res) => res.fullTextAnnotation);
+}
+
+/**
+ * processes the images -> transcriptions and store it in the DB
+ * it is up to the client to poll for any updates
+ */
+export function processTranscription(job_id, img_b64s) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      console.log("Job: " + job_id);
+      console.log("Total imgs: " + img_b64s.length);
+      const fullTextAnnotations = await visionOCR(img_b64s);
+      console.log(fullTextAnnotations[0].pages[0].blocks.length);
+      resolve();
+    } catch(e) {
+      reject(e);
     }
   });
 }
