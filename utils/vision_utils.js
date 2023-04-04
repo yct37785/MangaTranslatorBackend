3c4a2477920983d571e8b166478a1ed19a44ec2b
lumberman88 <yiechertan@gmail.com>
2023-04-05 00:55:10 +0800

    Batch process OCR as limit is 16 imgs per vision API call.

--- utils/vision_utils.js
+++ utils/vision_utils.js
@@ -1,13 +1,12 @@
 import vision from '@google-cloud/vision';
 import fs from 'fs';
-const IMG_PER_REQ = 2;
+const IMG_PER_REQ = 16;
+const client = new vision.ImageAnnotatorClient();
 
 /**
- * vision OCR
- * img_b64s: list of images in b64 format
+ * do vision API calls
  */
-export async function imageOCR(img_b64s) {
-
+async function callVision(img_b64s) {
   // detect text Vision API
   const imgRequests = img_b64s.map((b64) => {
     return {
@@ -18,9 +17,29 @@ export async function imageOCR(img_b64s) {
   const request = {
     requests: imgRequests
   };
-  const client = new vision.ImageAnnotatorClient();
   const [result] = await client.batchAnnotateImages(request);
+  return result;
+}
 
+/**
+ * vision OCR
+ * img_b64s: list of images in b64 format
+ */
+export async function imageOCR(img_b64s) {
+
+  // call api batch by batch
+  const batches = [[]];
+  for(let i = 0; i < img_b64s.length; ++i) {
+    if (batches[batches.length - 1].length == IMG_PER_REQ) {
+      batches.push([]);
+    }
+    batches[batches.length - 1].push(img_b64s[i]);
+  }
+  const res_list = await Promise.all(batches.map((batch) => callVision(batch)));
+  const result = { responses: [] };
+  for (let i = 0; i < res_list.length; ++i) {
+    result.responses = result.responses.concat(res_list[i].responses);
+  }
   // mock text data
   // const result = { responses: [] };
   // const data = fs.readFileSync('data/vision_data_rawkuma.json', 'utf8');
